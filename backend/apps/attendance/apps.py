ba09a996468690cb5082a87544afe0dc79602f6c
Stephen Gichimu <82092219+steve9948@users.noreply.github.com>
2025-10-18 01:43:07 +0300

    Fixed name error type in the attendance/apps.py file

--- backend/apps/attendance/apps.py
+++ backend/apps/attendance/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AttendanceConfig(AppCofig):
+class AttendanceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.attendance' # Match the path in INSTALLED_APPS
